6c4da7e620246c55d132d10f8c1cb80207b7bbbe
xyx <739386529@qq.com>
2021-12-07 23:35:11 +0800

    添加项目文件。

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,211 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.map = new GMap.NET.WindowsForms.GMapControl();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.longtitude = new System.Windows.Forms.TextBox();
+            this.latitude = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureZoomIn = new System.Windows.Forms.PictureBox();
+            this.pictureZoomOut = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureZoomIn)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureZoomOut)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // map
+            // 
+            this.map.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.map.Bearing = 0F;
+            this.map.CanDragMap = true;
+            this.map.EmptyTileColor = System.Drawing.Color.Navy;
+            this.map.GrayScaleMode = false;
+            this.map.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow;
+            this.map.LevelsKeepInMemmory = 5;
+            this.map.Location = new System.Drawing.Point(-81, -146);
+            this.map.MarkersEnabled = true;
+            this.map.MaxZoom = 2;
+            this.map.MinZoom = 2;
+            this.map.MouseWheelZoomEnabled = true;
+            this.map.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter;
+            this.map.Name = "map";
+            this.map.NegativeMode = false;
+            this.map.PolygonsEnabled = true;
+            this.map.RetryLoadTile = 0;
+            this.map.RoutesEnabled = true;
+            this.map.ScaleMode = GMap.NET.WindowsForms.ScaleModes.Integer;
+            this.map.SelectedAreaFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(65)))), ((int)(((byte)(105)))), ((int)(((byte)(225)))));
+            this.map.ShowTileGridLines = false;
+            this.map.Size = new System.Drawing.Size(1012, 973);
+            this.map.TabIndex = 0;
+            this.map.Zoom = 0D;
+            this.map.Load += new System.EventHandler(this.gMapControl1_Load);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(49, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "经度";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(49, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "纬度";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // longtitude
+            // 
+            this.longtitude.Location = new System.Drawing.Point(111, 30);
+            this.longtitude.Name = "longtitude";
+            this.longtitude.Size = new System.Drawing.Size(100, 25);
+            this.longtitude.TabIndex = 3;
+            this.longtitude.TextChanged += new System.EventHandler(this.longtitude_TextChanged);
+            // 
+            // latitude
+            // 
+            this.latitude.Location = new System.Drawing.Point(111, 72);
+            this.latitude.Name = "latitude";
+            this.latitude.Size = new System.Drawing.Size(100, 25);
+            this.latitude.TabIndex = 4;
+            this.latitude.TextChanged += new System.EventHandler(this.latitude_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(79, 119);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(96, 34);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "跳转";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.AllowNavigation = false;
+            this.webBrowser1.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.webBrowser1.IsWebBrowserContextMenuEnabled = false;
+            this.webBrowser1.Location = new System.Drawing.Point(-203, 404);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.ScrollBarsEnabled = false;
+            this.webBrowser1.Size = new System.Drawing.Size(459, 250);
+            this.webBrowser1.TabIndex = 6;
+            this.webBrowser1.Url = new System.Uri("https://hanyu.baidu.com/zici/s?wd=%E5%AF%84&query=%E5%AF%84&srcid=28232&from=kg0", System.UriKind.Absolute);
+            this.webBrowser1.WebBrowserShortcutsEnabled = false;
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Controls.Add(this.pictureZoomIn);
+            this.panel1.Controls.Add(this.pictureZoomOut);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.webBrowser1);
+            this.panel1.Controls.Add(this.longtitude);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.latitude);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Location = new System.Drawing.Point(955, 4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(259, 657);
+            this.panel1.TabIndex = 7;
+            // 
+            // pictureZoomIn
+            // 
+            this.pictureZoomIn.Image = global::WindowsFormsApp1.Properties.Resources.放大;
+            this.pictureZoomIn.Location = new System.Drawing.Point(43, 174);
+            this.pictureZoomIn.Name = "pictureZoomIn";
+            this.pictureZoomIn.Size = new System.Drawing.Size(43, 42);
+            this.pictureZoomIn.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureZoomIn.TabIndex = 9;
+            this.pictureZoomIn.TabStop = false;
+            this.pictureZoomIn.Click += new System.EventHandler(this.pictureZoomIn_Click);
+            // 
+            // pictureZoomOut
+            // 
+            this.pictureZoomOut.Image = global::WindowsFormsApp1.Properties.Resources.缩小;
+            this.pictureZoomOut.InitialImage = global::WindowsFormsApp1.Properties.Resources.缩小;
+            this.pictureZoomOut.Location = new System.Drawing.Point(143, 174);
+            this.pictureZoomOut.Name = "pictureZoomOut";
+            this.pictureZoomOut.Size = new System.Drawing.Size(45, 42);
+            this.pictureZoomOut.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureZoomOut.TabIndex = 8;
+            this.pictureZoomOut.TabStop = false;
+            this.pictureZoomOut.Click += new System.EventHandler(this.pictureZoomOut_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1226, 673);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.map);
+            this.Name = "Form1";
+            this.Text = "寄Map";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureZoomIn)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureZoomOut)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private GMap.NET.WindowsForms.GMapControl map;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox longtitude;
+        private System.Windows.Forms.TextBox latitude;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureZoomOut;
+        private System.Windows.Forms.PictureBox pictureZoomIn;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,255 @@
+﻿using GMap.NET;
+using GMap.NET.MapProviders;
+using GMap.NET.WindowsForms;
+using GMap.NET.WindowsForms.Markers;
+using GMapProvidersExt;
+using GMapWinForm;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public static GMapOverlay OverlayMarker;//放在全局使用
+        private GMapOverlay objects = new GMapOverlay("objects"); //放置marker的图层
+        private GMapMarkerImage currentMarker;
+        private bool isLeftButtonDown = false;
+        private Timer blinkTimer = new Timer();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //string mapPath = Application.StartupPath + "C:\\Users\\xyx\\source\\repos\\C#\\map\\map\\DataExp.gmdb";
+            //GMap.NET.GMaps.Instance.ImportFromGMDB(mapPath);
+            //gMapControl1.Manager.Mode = AccessMode.CacheOnly;//  ServerOnly,ServerAndCache设置从服务器和缓存中获取地图数据
+            //gMapControl1.MapProvider = AMapProvider.Instance;//GMapProviders.GoogleChinaMap;   //谷歌中国地图
+            map.MapProvider = GMapProviders.BingMap;
+            map.MinZoom = 1;      //最小比例
+            map.MaxZoom = 18;     //最大比例
+            map.Zoom = 8;        //当前比例
+            this.map.DragButton = System.Windows.Forms.MouseButtons.Left;//左键拖拽地图
+            map.MouseWheelZoomType = MouseWheelZoomType.MousePositionAndCenter;//鼠标缩放模式
+            //this.gMapControl1.SetPositionByKeywords("china,beijing");
+            this.map.Position= new PointLatLng(39.9, 116.47);
+            OverlayMarker = new GMapOverlay("OverlayMarker"); //创建一个名为“OverlayMarker”的图层
+            this.map.Overlays.Add(OverlayMarker);
+
+            //添加标记
+            DrawMarker(OverlayMarker, 39.9, 116.4);
+            PointLatLng start = new PointLatLng(38.264822, 120.357477);
+            PointLatLng end = new PointLatLng(38.164822, 120.357477);
+
+           GDirections myDirections;
+            RoutingProvider rp = map.MapProvider as RoutingProvider;
+            if (rp == null)
+            {
+                rp = GMapProviders.OpenStreetMap; // use OpenStreetMap if provider does not implement routing
+            }
+            map.MouseDown += new MouseEventHandler(gMapControl1_MouseDown);
+            map.MouseUp += new MouseEventHandler(gMapControl1_MouseUp);
+            map.MouseMove += new MouseEventHandler(gMapControl1_MouseMove);
+
+            map.OnMarkerClick += new MarkerClick(gMapControl1_OnMarkerClick);
+            map.OnMarkerEnter += new MarkerEnter(gMapControl1_OnMarkerEnter);
+            map.OnMarkerLeave += new MarkerLeave(gMapControl1_OnMarkerLeave);
+            MapRoute route = rp.GetRoute(start, end, false, false, (int)map.Zoom);
+            if (route == null)
+            {
+                //MessageBox.Show("寄Map！", "寄", MessageBoxButtons.OK);
+            }
+            GMapOverlay locations = new GMapOverlay();
+            GMapOverlay markersOverlay = new GMapOverlay("markers");
+            if (route != null)
+            {
+                // add route
+                GMapRoute r = new GMapRoute(route.Points, route.Name);
+                r.IsHitTestVisible = true;
+                locations.Routes.Add(r);
+
+                // add route start/end marks
+                GMapMarker m1 = new GMarkerGoogle(start, GMarkerGoogleType.green_big_go);
+                m1.ToolTipText = "Start: " + route.Name;
+                m1.ToolTipMode = MarkerTooltipMode.Always;
+
+                GMapMarker m2 = new GMarkerGoogle(end, GMarkerGoogleType.red_big_stop);
+                m2.ToolTipText = "End: " + end.ToString();
+                m2.ToolTipMode = MarkerTooltipMode.Always;
+
+                markersOverlay.Markers.Add(m1);
+                markersOverlay.Markers.Add(m2);
+                map.ZoomAndCenterRoute(r);
+                map.Overlays.Add(objects);
+            }
+        }
+        public void DrawMarker(GMapOverlay overlay, double lat, double lng)
+        {
+            //创建一个名为“overlay”的图层
+            //GMapOverlay overlay = new GMapOverlay("overlay");
+
+            //创建标记，并设置位置及样式
+            GMapMarker marker = new GMarkerGoogle(new PointLatLng(lat, lng), GMarkerGoogleType.blue_small);
+            //将标记添加到图层
+            overlay.Markers.Add(marker);
+            //将图层添加到地图
+            this.map.Overlays.Add(overlay);
+
+            //鼠标标记点提示框ToolTip
+            marker.ToolTip = new GMapToolTip(marker);
+            //Brush tooltipBackColor = new SolidBrush(Color.Transparent);//颜色获取，可用于填充背景
+            marker.ToolTipText = "实时坐标";
+            marker.ToolTip.Font = new Font("微软雅黑", 11);
+            marker.ToolTip.Fill = new SolidBrush(Color.FromArgb(100, Color.Black));
+            marker.ToolTip.Foreground = Brushes.White;
+            marker.ToolTip.TextPadding = new Size(20, 20);
+            //marker.ToolTipMode= MarkerTooltipMode.Always;//标注一直显示
+            marker.ToolTip.Offset = new System.Drawing.Point(marker.Offset.X - (int)((float)marker.ToolTipText.Length / 2) * 15, marker.Offset.Y + 28);//显示位置
+
+        }
+        void mapControl_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == System.Windows.Forms.MouseButtons.Left && isLeftButtonDown)
+            {
+                if (currentMarker != null)
+                {
+                    PointLatLng point = map.FromLocalToLatLng(e.X, e.Y);
+                    currentMarker.Position = point;
+                    currentMarker.ToolTipText = string.Format("{0},{1}", point.Lat, point.Lng);
+                }
+            }
+        }
+
+        private void gMapControl1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        void gMapControl1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == System.Windows.Forms.MouseButtons.Left && isLeftButtonDown)
+            {
+                if (currentMarker != null)
+                {
+                    PointLatLng point = map.FromLocalToLatLng(e.X, e.Y);
+                    currentMarker.Position = point;
+                    currentMarker.ToolTipText = string.Format("{0},{1}", point.Lat, point.Lng);
+                }
+            }
+        }
+
+        void gMapControl1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == System.Windows.Forms.MouseButtons.Left)
+            {
+                isLeftButtonDown = false;
+            }
+        }
+
+        void gMapControl1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == System.Windows.Forms.MouseButtons.Left)
+            {
+                isLeftButtonDown = true;
+            }
+        }
+
+        void gMapControl1_OnMarkerLeave(GMapMarker item)
+        {
+            if (item is GMapMarkerImage)
+            {
+                currentMarker = null;
+                GMapMarkerImage m = item as GMapMarkerImage;
+                m.Pen.Dispose();
+                m.Pen = null;
+            }
+        }
+
+        void gMapControl1_OnMarkerEnter(GMapMarker item)
+        {
+            if (item is GMapMarkerImage)
+            {
+                currentMarker = item as GMapMarkerImage;
+                currentMarker.Pen = new Pen(Brushes.Red, 2);
+            }
+        }
+
+        void gMapControl1_OnMarkerClick(GMapMarker item, MouseEventArgs e)
+        {
+        }
+
+        void gMapControl1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == System.Windows.Forms.MouseButtons.Right)
+            {
+                //objects.Markers.Clear();
+                PointLatLng point = map.FromLocalToLatLng(e.X, e.Y);
+                //GMapMarker marker = new GMarkerGoogle(point, GMarkerGoogleType.green);
+                Bitmap bitmap = Bitmap.FromFile("E:\\学习\\大三上\\C#\\GUI.png") as Bitmap;
+                //GMapMarker marker = new GMarkerGoogle(point, bitmap);
+                GMapMarker marker = new GMapMarkerImage(point, bitmap);
+                marker.ToolTipMode = MarkerTooltipMode.OnMouseOver;
+                marker.ToolTipText = string.Format("{0},{1}", point.Lat, point.Lng);
+                objects.Markers.Add(marker);
+            }
+        }
+
+        void gMapControl1_OnMapZoomChanged()
+        {
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double lat = latitude.Text.Length>0?Convert.ToDouble(latitude.Text):map.Position.Lat;
+            double lng = longtitude.Text.Length > 0 ? Convert.ToDouble(longtitude.Text) : map.Position.Lng;
+            PointLatLng p=new PointLatLng(lat,lng);
+            map.Position = p;
+            GMapMarker marker = new GMarkerGoogle(p, GMarkerGoogleType.blue_dot);
+            OverlayMarker.Markers.Add(marker);
+        }
+
+        private void longtitude_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void latitude_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void pictureZoomOut_Click(object sender, EventArgs e)
+        {
+            map.Zoom--;
+        }
+
+        private void pictureZoomIn_Click(object sender, EventArgs e)
+        {
+            map.Zoom++;
+        }
+    }
+}
